de90c388f566114816e92975c251a4801578a753
angelina.dorasheva <dorasheva.a@gmail.com>
2019-12-30 16:58:21 +0300

    Перепишет сортировку на классы

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,7 +1,29 @@
-export const createSortingTemplate = () => {
+import {createElement} from '../util.js';
+
+const createSortingTemplate = () => {
   return `<div class="board__filter-list">
     <a href="#" class="board__filter">SORT BY DEFAULT</a>
     <a href="#" class="board__filter">SORT BY DATE up</a>
     <a href="#" class="board__filter">SORT BY DATE down</a>
   </div>`;
 };
+
+export default class Sort {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSortingTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getElement());
+    }
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
